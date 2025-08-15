2282c14ba48672272e404cba4238e9afddcca5e6
kroy314-creator <kroy314@gmail.com>
2025-08-15 15:43:31 +0000

    prinovo commit Gabriel

---
+++ super_trunfo.c
@@ -0,0 +1,35 @@
+# super-trunfo-em-c-
+
+
+#include <stdio.h>
+
+CARTA 1:
+
+int main () {
+char Estado;
+char Códido da carta [];
+char Nome da Cidade [];
+int População [];
+float Área em km²;
+float PIB;
+int Números de pontos turísticos 
+
+return 0;
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
